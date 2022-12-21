3b8444bdeadab7f9da63f3d5117ac8c6c25b80fa
Oksana-Pak <okspak7@gmail.com>
2022-12-21 14:50:29 +0200

    add step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,27 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ContactForm } from 'components/ContactForm';
+import { ContactList } from 'components/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContact = contact => {
+    this.setState(({ contacts }) => ({
+      contacts: [contact, ...contacts],
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2>Contacts</h2>
+        {/* <Filter/> */}
+        <ContactList options={this.state.contacts} />
+      </div>
+    );
+  }
+}
