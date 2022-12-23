efd20c2cb74f118eb40052428a3e6c5489597ea5
Oksana-Pak <okspak7@gmail.com>
2022-12-23 12:38:27 +0200

    change id in Item

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,12 +1,11 @@
 import PropTypes from 'prop-types';
 import { ContactItem } from 'components/ContactItem';
-import { nanoid } from 'nanoid';
 
 export const ContactList = ({ options, onDeleteContact }) => (
   <ul>
     {options.map(({ id, name, number }) => (
       <ContactItem
-        key={nanoid()}
+        key={id}
         id={id}
         name={name}
         number={number}
